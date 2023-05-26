67efe0c027d31513ff5fa195e18ecf457fb1960c
TwelfthDannis <danyadannis@gmail.com>
2023-05-27 00:26:50 +0700

    add files

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+
+export function Main() {
+    return (
+        <div className={"container-fluid"}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnO43WrSdFWM8hDstFT1Hqb3Y3Sh4Fo8Mpi8Qz87_0yd1ukwHHvAYeFWx8KdZuZkY49vs&usqp=CAU" alt=""/>
+        </div>
+    );
+}
